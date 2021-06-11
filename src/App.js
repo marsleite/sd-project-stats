c9f6ff72696bd20ed87bf85086cb88a2ea060847
marsleite <marsleite@gmail.com>
2021-06-11 10:22:27 -0300

    instaalaaçao do react-router-dom e add conteudo ao componente react DestinyList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
+import DestinyList from './pages/DestinyList';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <span>Destiny Stats</span>
+      <header>
+        <h1>DESTINY CRUCIBLE STATS</h1>
       </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ DestinyList } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/DestinyList.jsx
+++ src/pages/DestinyList.jsx
@@ -3,8 +3,8 @@ import React,  { Component } from 'react';
 class DestinyList extends Component {
   render() {
     return (
-      <div>
-        <h1>Destiny Stats</h1>
+      <div className="App-header">
+        <h2>Destiny Stats here</h2>
       </div>
     )
   }
