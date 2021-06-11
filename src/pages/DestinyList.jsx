b93f9127aa30b618f92f473bed396362231b72a0
marsleite <marsleite@gmail.com>
2021-06-11 09:55:38 -0300

    criaçao dos arquivos iniciais do projeto

---
+++ src/pages/DestinyList.jsx
@@ -0,0 +1,13 @@
+import React,  { Component } from 'react';
+
+class DestinyList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Destiny Stats</h1>
+      </div>
+    )
+  }
+}
+
+export default DestinyList;
---
+++ src/pages/index.jsx
@@ -0,0 +1,1 @@
+export { default as DestinyList } from './DestinyList';
